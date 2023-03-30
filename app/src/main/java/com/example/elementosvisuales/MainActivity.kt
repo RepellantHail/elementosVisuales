efd2f2d22558301a0d96976047d205310492a518
RepellantHail <a20310068@ceti.mx>
2023-03-29 19:49:47 -0600

    Version de prueba

--- app/src/main/java/com/example/elementosvisuales/MainActivity.kt
+++ app/src/main/java/com/example/elementosvisuales/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +14,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var btntxt:Button
+        var txtInfo: EditText
         btntxt = findViewById(R.id.activity_maon_btnTexto)
+        txtInfo = findViewById(R.id.activity_main_txtInfo)
+
+        txtInfo.setOnEditorActionListener { textView, i, keyEvent ->
+            var handled = false
+            if(i == EditorInfo.IME_ACTION_SEARCH){
+                mostrarMensaje(textView.text.toString())
+                handled = true
+            }
+            handled
+        }
 
         btntxt.setOnClickListener(object: View.OnClickListener{
             override fun onClick(view: View){
@@ -24,6 +37,12 @@ class MainActivity : AppCompatActivity() {
 
     fun mostrarMensaje(){
         val toast = Toast.makeText(this@MainActivity, "Activado desde Código",Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.BOTTOM or Gravity.CENTER, 0,0)
+        toast.show()
+    }
+
+    fun mostrarMensaje(msj: String){
+        val toast = Toast.makeText(this@MainActivity, msj,Toast.LENGTH_LONG)
         toast.setGravity(Gravity.BOTTOM or Gravity.CENTER, 0,0)
         toast.show()
     }
